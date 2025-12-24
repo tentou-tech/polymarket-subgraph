78f5fe17e311ed5d5ca2c2803243bf488c6b6728
fibonacci998 <fibonacci998@gmail.com>
2025-12-24 15:05:39 +0700

    feat: add CTF Exchange entities and production scripts

--- src/ctf-exchange.ts
+++ src/ctf-exchange.ts
@@ -1,8 +1,65 @@
-import { OrdersMatched as OrdersMatchedEvent } from '../generated/CTFExchange/CTFExchange';
+import {
+  OrdersMatched as OrdersMatchedEvent,
+  TokenRegistered as TokenRegisteredEvent,
+  OrderFilled as OrderFilledEvent,
+} from '../generated/CTFExchange/CTFExchange';
 import { log } from '@graphprotocol/graph-ts';
 import { createEventOnchain } from './utils';
+import {
+  TokenRegistered,
+  OrderFilled,
+  OrdersMatched,
+} from '../generated/schema';
+
+export function handleTokenRegistered(event: TokenRegisteredEvent): void {
+  log.info('Handling TokenRegistered event', []);
+  let entity = new TokenRegistered(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.token0 = event.params.token0;
+  entity.token1 = event.params.token1;
+  entity.conditionId = event.params.conditionId;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleOrderFilled(event: OrderFilledEvent): void {
+  log.info('Handling OrderFilled event', []);
+  let entity = new OrderFilled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.orderHash = event.params.orderHash;
+  entity.maker = event.params.maker;
+  entity.taker = event.params.taker;
+  entity.makerAssetId = event.params.makerAssetId;
+  entity.takerAssetId = event.params.takerAssetId;
+  entity.makerAmountFilled = event.params.makerAmountFilled;
+  entity.takerAmountFilled = event.params.takerAmountFilled;
+  entity.fee = event.params.fee;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
 
 export function handleOrdersMatched(event: OrdersMatchedEvent): void {
   log.info('Handling OrdersMatched event', []);
-  createEventOnchain(event, 'OrdersMatched');
+  let entity = new OrdersMatched(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.takerOrderHash = event.params.takerOrderHash;
+  entity.takerOrderMaker = event.params.takerOrderMaker;
+  entity.makerAssetId = event.params.makerAssetId;
+  entity.takerAssetId = event.params.takerAssetId;
+  entity.makerAmountFilled = event.params.makerAmountFilled;
+  entity.takerAmountFilled = event.params.takerAmountFilled;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
--- src/uma-ctf-exchange.ts
+++ src/uma-ctf-exchange.ts
@@ -1,16 +1,43 @@
 import {
   QuestionInitialized as QuestionInitializedEvent,
-  QuestionResolved as QuestionResolvedEvent
+  QuestionResolved as QuestionResolvedEvent,
 } from '../generated/UmaCtfAdapter/UmaCtfAdapter';
 import { log } from '@graphprotocol/graph-ts';
 import { createEventOnchain } from './utils';
+import { QuestionInitialized, QuestionResolved } from '../generated/schema';
 
-export function handleQuestionInitialized(event: QuestionInitializedEvent): void {
+export function handleQuestionInitialized(
+  event: QuestionInitializedEvent
+): void {
   log.info('Handling QuestionInitialized event', []);
-  createEventOnchain(event, 'QuestionInitialized');
+  let entity = new QuestionInitialized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.questionID = event.params.questionID;
+  entity.requestTimestamp = event.params.requestTimestamp;
+  entity.creator = event.params.creator;
+  entity.ancillaryData = event.params.ancillaryData;
+  entity.rewardToken = event.params.rewardToken;
+  entity.reward = event.params.reward;
+  entity.proposalBond = event.params.proposalBond;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
 
 export function handleQuestionResolved(event: QuestionResolvedEvent): void {
   log.info('Handling QuestionResolved event', []);
-  createEventOnchain(event, 'QuestionResolved');
+  let entity = new QuestionResolved(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.questionID = event.params.questionID;
+  entity.settledPrice = event.params.settledPrice;
+  entity.payouts = event.params.payouts;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
